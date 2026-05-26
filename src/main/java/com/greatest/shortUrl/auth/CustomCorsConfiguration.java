fe3818de9f04d7dcc367fbc2c63ffe138582afc4
sagr612 <levieren111@gmail.com>
2026-05-26 21:14:00 +0530

    cors fixed

--- src/main/java/com/greatest/shortUrl/auth/CustomCorsConfiguration.java
+++ src/main/java/com/greatest/shortUrl/auth/CustomCorsConfiguration.java
@@ -7,14 +7,34 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+
+        config.setAllowedOrigins(
+                List.of(
+                        "http://localhost:5173",
+                        frontendUrl
+                )
+        );
+
+        config.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        );
+
         config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
+
         return config;
     }
 }
